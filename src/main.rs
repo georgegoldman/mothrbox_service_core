c49574332a910ff7d9f56a5d76a29b4cf7f03136
georgegoldman <georgegoldmanjohn.o@gmail.com>
2025-04-04 17:43:17 +0100

    implemented the generate_keypair fn

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use rand::rngs::OsRng;
-use x25519_dalek::EphemeralSecret;
-use x25519_dalek::PublicKey;
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Nonce,
+};
+use p256::{
+    ecdh::EphemeralSecret, EncodedPoint, PublicKey, SecretKey
+};
+use rand_core::RngCore;
+use rayon::prelude::*;
+use sha2::{Digest, Sha256};
+use p256::ecdsa::VerifyingKey;
 
-fn generate_key_pair() -> (EphemeralSecret, PublicKey) {
-    let private_key = EphemeralSecret::random_from_rng(OsRng);
-    let public_key = PublicKey::from(&private_key);
-    (private_key, public_key)
+fn generate_keypair() -> (SecretKey, PublicKey) {
+    let secret = SecretKey::random(&mut OsRng);
+    let public = secret.public_key();
+    (secret, public)
 }
 
-fn derive_shared_secret(my_private: &EphemeralSecret, peer_public: &PublicKey) -> [u8; 32] {
-    my_private.diffie_hellman(peer_public).to_bytes()
-}
-
-fn main()
-{
-    // generate key 
+fn main() {
+    println!("{:?}", generate_keypair())
 }
